23b55a5cbff1ff9e4b4859937a29dfca77391ebe
学良 钟 <287175720@qq.com>
2021-07-05 11:28:12 +0800

    hanshu_moban

---
+++ moban/hanshu_moban/hanshu_moban/hanshu_moban.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+template <class T>
+void allprint(T *a,int size) {
+	for (int i = 0; i < size; i++) {
+		cout << a[i];
+		if (i < (size - 1)) {
+			cout << ", ";
+		}
+	}
+	cout << endl;
+}
+
+
+
+
+int main() {
+	const int MAX = 5;
+	int a[MAX] = {1, 2, 3, 4, 5};
+	double b[MAX] = { 1.1,2.2,3.3,4.4,5.5 };
+	const char *c[MAX] = { "abcd","cdef","dwdw","adad","adwd"};
+
+	allprint(a, MAX);
+	allprint(b, MAX);
+	allprint(c, MAX);
+	
+	return 0;
+}
