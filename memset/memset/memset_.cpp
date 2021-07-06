17eb5e0a58377226395bfd273c60c6cd9bc3c531
学良 钟 <287175720@qq.com>
2021-07-06 20:02:34 +0800

    memset 和 sizeof内置类型的值。

---
+++ memset/memset/memset_.cpp
@@ -0,0 +1,19 @@
+/* memset example */
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	char str[] = "almost every programmer should know memset!";
+	memset(str, '-', 6);
+	puts(str);
+	cout << str << endl;
+
+
+
+
+	return 0;
+
+
+}
---
+++ memset/memset/sizeof_T.cpp
@@ -0,0 +1,13 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+int main() {
+	cout << "sizeof(float)= " << sizeof(float) << endl;//4
+	cout << "sizeof(int)= " << sizeof(int) << endl;//4
+	cout << "sizeof(double)= " << sizeof(double) << endl;//8
+	cout << "sizeof(char)= " << sizeof(char) << endl;//1
+	return 0;
+}
