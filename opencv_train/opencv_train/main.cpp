270c121cd0bd300ebdef66e0e3ba74d09ed13940
学良 钟 <287175720@qq.com>
2021-07-13 20:02:44 +0800

    手动卷积、filter2D

--- opencv_train/opencv_train/main.cpp
+++ opencv_train/opencv_train/main.cpp
@@ -4,6 +4,7 @@
 #include<opencv2/core/mat.hpp>
 #include<opencv2/imgcodecs/imgcodecs.hpp>
 #include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
 using namespace std;
 using namespace cv;
 
@@ -137,49 +138,49 @@ using namespace cv;
 //}
 
 //////////////////////////////////////////////using Mat::at() for pass through image
-Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* const table) {
-	//  accept only char type matrices
-	CV_Assert(I.depth() == CV_8U);
-
-	const int channels = I.channels();
-
-	switch (channels)
-	{
-	case 1:
-	{
-		for (int i = 0; i < I.rows; ++i) {
-			for (int j = 0; j < I.cols; ++j) {
-				I.at<uchar>(i, j) = table[I.at<uchar>(i, j)];
-			}
-
-		}
-
-		
-		break;
-	}
-
-	case 3:
-	{
-		Mat_<Vec3b> _I = I;
-
-		for (int i = 0; i < I.rows; ++i) {
-			for (int j = 0; j < I.cols; ++j) {
-				_I(i, j)[0] = table[_I(i, j)[0]];
-				_I(i, j)[1] = table[_I(i, j)[1]];
-				_I(i, j)[2] = table[_I(i, j)[2]];
-			}
-		}
-
-		I = _I;
-		break;
-
-	}
-
-	}
-	return I;
-
-
-}
+//Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* const table) {
+//	//  accept only char type matrices
+//	CV_Assert(I.depth() == CV_8U);
+//
+//	const int channels = I.channels();
+//
+//	switch (channels)
+//	{
+//	case 1:
+//	{
+//		for (int i = 0; i < I.rows; ++i) {
+//			for (int j = 0; j < I.cols; ++j) {
+//				I.at<uchar>(i, j) = table[I.at<uchar>(i, j)];
+//			}
+//
+//		}
+//
+//		
+//		break;
+//	}
+//
+//	case 3:
+//	{
+//		Mat_<Vec3b> _I = I;
+//
+//		for (int i = 0; i < I.rows; ++i) {
+//			for (int j = 0; j < I.cols; ++j) {
+//				_I(i, j)[0] = table[_I(i, j)[0]];
+//				_I(i, j)[1] = table[_I(i, j)[1]];
+//				_I(i, j)[2] = table[_I(i, j)[2]];
+//			}
+//		}
+//
+//		I = _I;
+//		break;
+//
+//	}
+//
+//	}
+//	return I;
+//
+//
+//}
 
 //void main() {
 //	int divideWith = 10; // convert our input string to number - C++ style
@@ -211,35 +212,150 @@ Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* const table) {
 /// <summary>
 /// /////////////////////////////////////////the fast way without writing the logic of scanning image .
 /// </summary>
+//void main() {
+//	
+//	int divideWith = 10; // convert our input string to number - C++ style
+//	uchar table[256];
+//	for (int i = 0; i < 256; ++i)
+//		table[i] = (uchar)(divideWith * (i / divideWith));
+//
+//	//![table-init]
+//	Mat lookUpTable(1, 256, CV_8U);//构建一个Mat类型变量，1行256列。8位uchar
+//	uchar* p = lookUpTable.ptr();
+//	for (int i = 0; i < 256; ++i) {
+//		p[i] = table[i];//通过ptr（）指针，将table[i]的值赋给lookUpTable矩阵
+//	}
+//
+//	Mat c = imread("Resources/waitSR.jpg");
+//	if (c.data == NULL) {
+//		cout << "read failed" << endl;
+//		exit(1);
+//	}
+//
+//	Mat J;
+//	LUT(c, lookUpTable, J);
+//	imshow("before ", c);
+//	waitKey(0);
+//	imshow("after ", J);
+//	waitKey(0);
+//	//! [table-init]
+//}
 
-void main() {
+
+////////////////////////////////////////////////////////!卷积初试
+void Sharpen(const Mat& myImage, Mat& Result);
+
+int main(int argc, char* argv[]) {
+	/*
+	Hand written function time passed in seconds: 0.008001
+	Built-in filter2D time passed in seconds:  0.0028362
+	*/
+
+	const char* filename = argc >= 2 ? argv[1] : "waitSR.jpg";
 	
-	int divideWith = 10; // convert our input string to number - C++ style
-	uchar table[256];
-	for (int i = 0; i < 256; ++i)
-		table[i] = (uchar)(divideWith * (i / divideWith));
+	Mat src, dst0, dst1;
 
-	//![table-init]
-	Mat lookUpTable(1, 256, CV_8U);//构建一个Mat类型变量，1行256列。8位uchar
-	uchar* p = lookUpTable.ptr();
-	for (int i = 0; i < 256; ++i) {
-		p[i] = table[i];//通过ptr（）指针，将table[i]的值赋给lookUpTable矩阵
+	src = imread(samples::findFile(filename), IMREAD_COLOR);
+
+	if (src.empty())
+	{
+		cerr << "Cant open image [" << filename << "]" << endl;
+		return EXIT_FAILURE;
 	}
 
-	Mat c = imread("Resources/waitSR.jpg");
-	if (c.data == NULL) {
-		cout << "read failed" << endl;
-		exit(1);
-	}
+	namedWindow("Input", WINDOW_AUTOSIZE);
+	namedWindow("Output", WINDOW_AUTOSIZE);/////??????????
 
-	Mat J;
-	LUT(c, lookUpTable, J);
-	imshow("before ", c);
-	waitKey(0);
-	imshow("after ", J);
-	waitKey(0);
-	//! [table-init]
+	imshow("Input", src);
+	double t = (double)getTickCount();
+
+	Sharpen(src, dst0);
+
+	t = ((double)getTickCount() - t) / getTickFrequency();
+	cout << "Hand written function time passed in seconds: " << t << endl;
+
+
+	imshow("Output", dst0);
+	waitKey();
+
+
+	Mat kernel = (Mat_<char>(3, 3) << 
+		0, -1, 0,
+		-1, 5, -1,
+		0, -1, 0);
+
+	t = (double)getTickCount();
+	filter2D(src, dst1, src.depth(), kernel);
+	t = ((double)getTickCount() - t) / getTickFrequency();
+	cout << "Built-in filter2D time passed in seconds:  " << t << endl;
+
+	imshow("Output2", dst1);
+	waitKey();
+
+	/*filter2D()，该函数甚至还有第五个可选参数来指定内核的中心，第六个参数用于在将过滤后的像素存储到K中之前向它们添加一个可选值，第七个参数用于决定在未定义操作的区域(边界)中执行什么操作。*/
+
+
+
+	return EXIT_SUCCESS;
 }
+
+
+void Sharpen(const Mat& myImage, Mat& Result) {
+	CV_Assert(myImage.depth() == CV_8U);//accept only uchar images
+
+	const int nchannels = myImage.channels();
+	Result.create(myImage.size(), myImage.type());
+
+	for (int j = 1; j < myImage.rows - 1; ++j) {
+		const uchar* previous = myImage.ptr<uchar>(j - 1);
+		const uchar* current = myImage.ptr<uchar>(j);
+		const uchar* next = myImage.ptr<uchar>(j + 1);
+
+		uchar* output = Result.ptr<uchar>(j);
+
+		for (int i = nchannels; i < nchannels * (myImage.cols - 1); ++i)
+		{
+			*output++ = saturate_cast<uchar>(
+				5 * current[i] - current[i - nchannels] - current[i + nchannels] - previous[i] - next[i]);
+			//每次算术操作后，移动指针一字节。它的确是从第一行，第一列开始的。而不是像current那样
+		}
+
+	}
+	//cout << Result << endl;
+	Result.row(0).setTo(Scalar(0));
+	Result.row(Result.rows - 1).setTo(Scalar(0));
+	Result.col(0).setTo(Scalar(0));
+	Result.col(Result.cols - 1).setTo(Scalar(0));
+	//但即使不置零，图像看起来也没什么差别。
+	//On the borders of the image
+	//the upper notation results inexistent pixel locations(like minus one - minus one).
+	//In these points our formula is undefined.
+	//A simple solution is to not apply the kernel in these pointsand, for example, set the pixels on the borders to zeros :
+
+}
+//////!卷积初试
+
+
+////////////////////////////////////////!ptr<>()
+//void main() {
+//	Mat j(10, 10, CV_8UC3,Scalar(1,1,3)),result;
+//	Sharpen(j, result);
+//	cout <<endl<< result << endl;
+//	//cout << j << endl;
+//}
+//////////////!ptr<>()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
