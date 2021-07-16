200d206d789a3cf5283895b1aa1905a53f7eb8ef
学良 钟 <287175720@qq.com>
2021-07-16 20:27:07 +0800

    convolution_3x3depthwise()、CDataBlob,Filters、convolutionDP()、vecAdd()

--- AVX2/AVX2/AVX2.cpp
+++ AVX2/AVX2/AVX2.cpp
@@ -1,5 +1,15 @@
 #include <iostream>	
 #include <immintrin.h>
+
+#ifndef MAX
+#define MAX(a,b) ((a)<(b)?(b):(a))
+#endif // !MAX
+
+#ifndef MIN
+#define MIN(a,b) ((a)>(b)?(b):(a))
+#endif // !MIN
+
+
 using namespace std;
 
 
@@ -52,11 +62,33 @@ inline bool vecMulAdd(const float* p1, const float* p2, float* p3, int num) {
 	}
 }
 
-template <class T>
-class CDataBlob {};
+inline bool vecAdd(const float * p1, float* p2, int num) {//p1指向滤波器偏差，p2指向输出图像某元素 的通道
+	__m256 a_float_x8, b_float_x8;
+	for (int i = 0; i < num; i += 8) {
+		a_float_x8 = _mm256_load_ps(p1 + i);
+		b_float_x8 = _mm256_load_ps(p2 + i);
+		b_float_x8 = _mm256_add_ps(a_float_x8, b_float_x8);
+		_mm256_store_ps(p2 + i, b_float_x8);
+	}
+}
 
 template <class T>
-class Filters {};
+class CDataBlob {
+public:
+	int rows;
+	int cols;
+
+	void setzero();
+	T * ptr(int r, int c);
+};
+
+template <class T>
+class Filters {
+public:
+	int num_filters;
+	CDataBlob<T>  weights;
+
+};
 bool convolution(CDataBlob<float>& inputData, Filters<float>& filtersP, CDataBlob<float>& outputData,bool do_relu);
 
 
@@ -79,10 +111,47 @@ bool convolution4layerUnit(CDataBlob<float>& inputData,
 	bool r1 = convolutionDP(inputData, filtersP1, filtersD1, tmp, true);
 	bool r2 = convolutionDP(tmp, filtersP2, filtersD2, outputData, do_relu);
 	return r1 && r2;
+}
 
 
+bool convolution_3x3depthwise(CDataBlob<float>&inputData, Filters<float>&filters, CDataBlob<float>&outputData) {
+	//set all elements in outputData to zeros
+	outputData.setzero();
 
+	for (int row = 0; row < outputData.rows; row++) {
+		int srcy_start = row - 1;//选取滤波器对应的输入图像的范围。 
+		int srcy_end = srcy_start + 3;//一共三行
+		srcy_start = MAX(0, srcy_start);//有可能会超过输入图像 的上边缘
+		srcy_end = MIN(srcy_end, inputData.rows);//有可能会超过 输入图像的下边缘
 
+		for (int col = 0; col < outputData.cols; col++) {
+			int srcx_start = col - 1;
+			int srcx_end = srcx_start + 3;
+			srcx_start = MAX(0, srcx_start);
+			srcx_end = MIN(srcx_end, inputData.cols);
+
+			float* pOut = outputData.ptr(row, col);//指向输出图像的某行某列
+
+			//定义一个滤波器，3x3，他的中心元素位于输入图像的第0元素，该行的上一行，该列的左一列。
+			for (int r = srcy_start; r < srcy_end; r++) {//r的活动范围在一个滑动窗口
+				for (int c = srcx_start; c < srcx_end; c++) {
+					//选取滤波器对应的输入图像的范围。
+					
+					int filter_r = r - row + 1;//滤波器起始位置为0行
+					int filter_c = c - col + 1;//滤波器起始位置为0列
+					int filter_idx = filter_r * 3 + filter_c;//坐标
+
+					//输入图像的通道默认于滤波器的输入通道相同？ 输出图像的通道与滤波器个数相同。
+					vecMulAdd(inputData.ptr(r, c), filters.weights.ptr(0,filter_idx),pOut, filters.num_filters);//输入图像第一个元素与滤波器对应元素的所有通道相乘，暂存。
+					//第二个元素与滤波器对应元素的所有通道相乘，再与第一次的数据相加。以此类推，直到滤波器对应的9个元素都操作过了，那么就是执行了卷积的对应位相乘相加，得到输出图像的第某行
+					//第某列元素的所有通道。
+				}
+			}
+			vecAdd();//将输出图像的第某行第某列元素的所有通道加上对应的偏差。
+
+		}
+	}
+}
 
 
 
