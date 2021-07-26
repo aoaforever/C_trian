8bc039ca1e6cbc5a26804aaa5f58dbec3d942544
学良 钟 <287175720@qq.com>
2021-07-26 10:02:28 +0800

    卷积参数更正，3x3变成1x1

--- super_resolution/super_resolution/superResolution_data.cpp
+++ super_resolution/super_resolution/superResolution_data.cpp
@@ -1,9 +1,14 @@
 #include"superResolution.h"
 
-float f1_weight[3 * 1 * 3 * 3] = { 0,0,0,-1,-1,-1,0,0,0,-1,-1,-1,5,5,5,-1,-1,-1,0,0,0,-1,-1,-1,0,0,0 };
-float f1_bias[3] = { 0,0,0 };
+float f1_weight[3 * 32 * 1 * 1] = { 0,-1,0,-1,5,-1,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+									0,0,0,0,0,0,0,0,0,0,0,0,-1,0,-1,5,-1,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,
+									0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,-1,5,-1,0,-1,0};
+float f1_bias[3] = {0,0,0 };
 
+
+
+//[channels,num_filters,is_depthwise,is_pointwise,with_relu,pWeight,pBiases]
 ConvInfoStruct param_pConvInfo[1] = {
-	{3,3,true,false,false,f1_weight,f1_bias}
+	{32,3,false,true,false,f1_weight,f1_bias}
 };
 
