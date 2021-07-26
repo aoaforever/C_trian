3ba27c7e3c86354f263167b273241cfd0d6da042
学良 钟 <287175720@qq.com>
2021-07-26 10:04:16 +0800

    添加卷积后将图像元素一个一个的添加到Mat类上，打印出来。但是R通道的值比较大，整体成红色，还有色彩溢出的问题，自己定义了一个阈值器。

--- super_resolution/super_resolution/superResolution_model.cpp
+++ super_resolution/super_resolution/superResolution_model.cpp
@@ -3,7 +3,20 @@
 #include<string.h>
 
 #define NUM_CONV_LAYER 1
-#define test 0
+#define test111 1
+
+
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/core/mat.hpp>
+#include<opencv2/imgcodecs/imgcodecs.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/hal/interface.h>
+using namespace cv;
+using namespace std;
+#define compare(a) ((a)>=(255)?(255):((a)<(0)?(0):(a)))
+
 extern ConvInfoStruct param_pConvInfo[NUM_CONV_LAYER];
 Filters<float> g_pFilters[NUM_CONV_LAYER];
 
@@ -20,8 +33,8 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 	CDataBlob<float> dataBlobs[2];
 
 	if (!param_initialized) {
-#if test
-		cout << "doing param_initialized" << endl;
+#if test111
+		std::cout << "doing param_initialized" << endl;
 #endif
 		init_parameters();
 	}
@@ -29,6 +42,36 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 	dataBlobs[0].convertDatatoCDataBlob(rgbImageData, width, height, 3, step);
 
 	//group convolution 3x3 
+#if test111
+	std::cout << g_pFilters[0].weights;
+	cout << dataBlobs[0].channels;
+#endif
+
 	convolution(dataBlobs[0], g_pFilters[0], dataBlobs[1], false);
 
+
+
+#if test111
+	Mat result(dataBlobs[1].rows, dataBlobs[1].cols, CV_8UC3);
+	for (int r = 0; r < result.rows; r++)
+	{
+		uchar* p = (uchar*)result.ptr(r);
+		
+		for (int c = 0; c < result.cols; c++)
+		{
+			for (int ch = 0; ch < result.channels(); ch++)
+			{
+				int idx = c * 3+ch;
+				//cout << compare(dataBlobs[1].getElement(r, c, ch)) <<endl;
+				p[idx] =compare(dataBlobs[1].getElement(r, c, ch));
+				
+			}
+
+			//cout << endl;
+		}
+	}
+
+	imshow("aaa", result);
+	waitKey(0);
+#endif
 }
