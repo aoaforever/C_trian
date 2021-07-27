5ccafcae7b3364ecb2bbc1a4fdad8041143cabd4
学良 钟 <287175720@qq.com>
2021-07-27 10:27:21 +0800

    添加读入IMREAD_UNCHANGED标志，添加打印像素测试代码。

--- super_resolution/super_resolution/SR_image.cpp
+++ super_resolution/super_resolution/SR_image.cpp
@@ -4,13 +4,30 @@
 
 using namespace cv;
 using namespace std;
+#define test112 1
+
 
 int main() {
-	Mat image = imread("Resources/waitSR.jpg");
+	Mat image = imread("Resources/waitSR.jpg",IMREAD_UNCHANGED);
 	if (image.empty()) {
 		cerr << "Can not load the image file :" << "waitSR.jpg" << endl;
 		return -1;
 	}
+
+	////´òÓ¡Í¼ÏñÖµ
+#if test112
+		for (int ch = 0; ch < image.channels(); ch++) {
+			cout << "The original input \n" << ch << " channel= (\n" << (int)image.at<Vec3b>(0, 0)[ch] << "\t" << (int)image.at<Vec3b>(0, 1)[ch] << " ... " << (int)image.at<Vec3b>(0, image.cols - 1)[ch] << endl
+				<< (int)image.at<Vec3b>(1, 0)[ch] << "\t" << (int)image.at<Vec3b>(1, 1)[ch] << " ... " << (int)image.at<Vec3b>(1, image.cols-1)[ch] << endl
+				<< " , ..., " << endl
+				<< (int)image.at<Vec3b>(image.rows-1, 0)[ch] << "\t" << (int)image.at<Vec3b>(image.rows-1, 1)[ch] << " ... " << (int)image.at<Vec3b>(image.rows-1, image.cols-1)[ch] << endl
+
+				<< " )" << endl;
+		}
+
+#endif
+
+
 	imshow("asd", image);
 	waitKey(0);
 	superResolution(image.ptr(0), image.cols, image.rows, (int)image.step);
