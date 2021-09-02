ea0cbc6401d92d51dcca803658cd0420f67066f0
学良 钟 <287175720@qq.com>
2021-09-02 10:45:06 +0800

    没啥好写

--- super_resolution/super_resolution/superResolution_model.cpp
+++ super_resolution/super_resolution/superResolution_model.cpp
@@ -3,7 +3,7 @@
 #include<string.h>
 
 #define NUM_CONV_LAYER 2
-#define test111 0
+#define test111 1
 
 
 
@@ -33,7 +33,7 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 	CDataBlob<float> dataBlobs[NUM_CONV_LAYER+1];
 
 	if (!param_initialized) {
-#if test111
+#if test1112
 		std::cout << "doing param_initialized" << endl;
 #endif
 		init_parameters();
@@ -42,39 +42,45 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 	dataBlobs[0].convertDatatoCDataBlob(rgbImageData, width, height, 3, step);
 
 	//group convolution 3x3 
-#if test111
+#if test1112
 	//std::cout << g_pFilters[0].weights;
 	cout << dataBlobs[0];
 #endif
 
 	convolution(dataBlobs[0], g_pFilters[0], dataBlobs[1], false);
 	convolution(dataBlobs[1], g_pFilters[1], dataBlobs[2], false);
-#if test111
+#if test1112
 	//std::cout << g_pFilters[0].weights;
 	cout << dataBlobs[1];
 #endif
 
 #if test111
-	Mat result(dataBlobs[1].rows, dataBlobs[1].cols, CV_8UC3);
-	for (int r = 0; r < result.rows; r++)
+	Mat result1(dataBlobs[1].rows, dataBlobs[1].cols, CV_8UC3);
+	Mat result2(dataBlobs[2].rows, dataBlobs[2].cols, CV_8UC3);
+	for (int r = 0; r < result2.rows; r++)
 	{
-		uchar* p = (uchar*)result.ptr(r);
-		
-		for (int c = 0; c < result.cols; c++)
+		uchar* p1 = (uchar*)result1.ptr(r);
+		uchar* p2 = (uchar*)result2.ptr(r);
+		for (int c = 0; c < result2.cols; c++)
 		{
-			for (int ch = 0; ch < result.channels(); ch++)
+			for (int ch = 0; ch < result2.channels(); ch++)
 			{
 				int idx = c * 3+ch;
 				//cout << compare(dataBlobs[1].getElement(r, c, ch)) <<endl;
-				p[idx] =compare(dataBlobs[1].getElement(r, c, ch));
-				
+				p1[idx] =compare(dataBlobs[1].getElement(r, c, ch));
+				p2[idx] = compare(dataBlobs[2].getElement(r, c, ch));
 			}
 
 			//cout << endl;
 		}
 	}
+	//namedWindow("conv1", 0);
+	//namedWindow("conv2", 0);
+	//resizeWindow("conv1", dataBlobs[1].cols / 1.8, dataBlobs[1].rows / 1.8);
+	//resizeWindow("conv2", dataBlobs[2].cols / 1.8, dataBlobs[2].rows / 1.8);
 
-	//imshow("aaa", result);
-	//waitKey(0);
+	imshow("conv1", result1);
+	imshow("conv2", result2);
+	waitKey(0);
 #endif
 }
