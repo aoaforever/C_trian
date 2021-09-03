0fdac67dcdfe9be60e44c520b655ccdc55afcb54
学良 钟 <287175720@qq.com>
2021-09-03 19:54:32 +0800

    测试pixelShuffle()

--- super_resolution/super_resolution/superResolution_model.cpp
+++ super_resolution/super_resolution/superResolution_model.cpp
@@ -48,26 +48,28 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 #endif
 
 	convolution(dataBlobs[0], g_pFilters[0], dataBlobs[1], false);
-	convolution(dataBlobs[1], g_pFilters[1], dataBlobs[2], false);
+	pixelShuffle(dataBlobs[1], dataBlobs[2], 2);
+	//convolution(dataBlobs[1], g_pFilters[1], dataBlobs[2], false);
 #if test1112
 	//std::cout << g_pFilters[0].weights;
 	cout << dataBlobs[1];
 #endif
 
 #if test111
-	Mat result1(dataBlobs[1].rows, dataBlobs[1].cols, CV_8UC3);
+	//Mat result1(dataBlobs[1].rows, dataBlobs[1].cols, CV_8UC3);
 	Mat result2(dataBlobs[2].rows, dataBlobs[2].cols, CV_8UC3);
+	cout << dataBlobs[2].rows << " and " << dataBlobs[2].cols << " and " << dataBlobs[2].channels << endl;
 	for (int r = 0; r < result2.rows; r++)
 	{
-		uchar* p1 = (uchar*)result1.ptr(r);
+		//uchar* p1 = (uchar*)result1.ptr(r);
 		uchar* p2 = (uchar*)result2.ptr(r);
 		for (int c = 0; c < result2.cols; c++)
 		{
 			for (int ch = 0; ch < result2.channels(); ch++)
 			{
-				int idx = c * 3+ch;
+				int idx = c * 2+ch;
 				//cout << compare(dataBlobs[1].getElement(r, c, ch)) <<endl;
-				p1[idx] =compare(dataBlobs[1].getElement(r, c, ch));
+				//p1[idx] =compare(dataBlobs[1].getElement(r, c, ch));
 				p2[idx] = compare(dataBlobs[2].getElement(r, c, ch));
 			}
 
@@ -79,7 +81,7 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 	//resizeWindow("conv1", dataBlobs[1].cols / 1.8, dataBlobs[1].rows / 1.8);
 	//resizeWindow("conv2", dataBlobs[2].cols / 1.8, dataBlobs[2].rows / 1.8);
 
-	imshow("conv1", result1);
+	//imshow("conv1", result1);
 	imshow("conv2", result2);
 	waitKey(0);
 #endif
