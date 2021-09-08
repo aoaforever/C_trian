0ffb88587956322e382b14c27b0777cc8bf55c32
“学良 <287175720@qq.com>
2021-09-08 14:41:49 +0800

    修复将CDataBlob类转化为Mat类的指针指向问题。并且得到了前向推理的正确结果。

--- super_resolution/super_resolution/superResolution_model.cpp
+++ super_resolution/super_resolution/superResolution_model.cpp
@@ -28,16 +28,17 @@ void show(CDataBlob<float>& dataBlobs) {
 	cout << dataBlobs.rows << " and " << dataBlobs.cols << " and " << dataBlobs.channels << endl;
 	for (int r = 0; r < result2.rows; r++)
 	{
-		//uchar* p1 = (uchar*)result1.ptr(r);
-		uchar* p2 = (uchar*)result2.ptr(r);
+		
 		for (int c = 0; c < result2.cols; c++)
 		{
+			
 			for (int ch = 0; ch < result2.channels(); ch++)
 			{
 				int idx = c * 2 + ch;
+		
 				//cout << compare(dataBlobs[1].getElement(r, c, ch)) <<endl;
 				//p1[idx] =compare(dataBlobs[1].getElement(r, c, ch));
-				p2[idx] = compare(dataBlobs.getElement(r, c, 2 - ch) * 255.0);
+				result2.at<Vec3b>(r,c)[ch] = compare(dataBlobs.getElement(r, c, 2 - ch) * 255.0);
 			}
 
 			//cout << endl;
@@ -94,18 +95,19 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 	//head
 	cout << "head conv\n";
 	convolution(dataBlobs[0], g_pFilters[0], dataBlobs[1], false);
-	cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n" << dataBlobs[1] << "\n\n\n\n\n\n";
+	//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\nhead conv\n" << dataBlobs[1] << "\n\n\n\n\n\n";
 	//show(dataBlobs[1]);
 	cout << "head conv done\n";
 	//simple_blocks x10
 	cout << "simple_blocks x10 conv\n";
 	int temp = 1;
 	for(int i =1;i<=10;i++){
-		//cout << i << endl;
+		cout << i <<"," << temp << ", " << temp + 1 << ',' << temp + 2 << ',' << temp + 3 << ", " << i + 1 << endl;
 		convolutionforsimpleblocks(dataBlobs[i], g_pFilters[temp], g_pFilters[temp +1], g_pFilters[temp +2], g_pFilters[temp +3], dataBlobs[i+1], true);
 		PixelAdd(dataBlobs[i], dataBlobs[i + 1]);
+		//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n 1个 simple_blocks \n" << dataBlobs[1] << "\n\n\n\n\n\n";
 		//cout << dataBlobs[i] << endl << endl;
-		temp = i+4;
+		temp = temp+4;//1111111111111111111111111111111111111111111111111111
 	}
 	cout << "simple_blocks x10 conv done\n";
 	/*convolutionforsimpleblocks(dataBlobs[1], g_pFilters[1], g_pFilters[2], g_pFilters[3], g_pFilters[4], dataBlobs[2], true);
@@ -114,14 +116,14 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 	convolutionforsimpleblocks(dataBlobs[4], g_pFilters[13], g_pFilters[14], g_pFilters[15], g_pFilters[16], dataBlobs[5], true);*/
 	cout << "before pix conv\n";
 	convolution(dataBlobs[11], g_pFilters[41], dataBlobs[12], false);	
-	cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n" << dataBlobs[12] << "\n\n\n\n\n\n";
+	//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n上采样前\n" << dataBlobs[12] << "\n\n\n\n\n\n";
 	cout << "before pix conv done\n";
 	cout << "pixelShuffle doing\n";
 	pixelShuffle(dataBlobs[12], dataBlobs[13], 2);
-	//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n" << dataBlobs[13] << "\n\n\n\n\n\n";
+	//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n上采样后\n" << dataBlobs[13] << "\n\n\n\n\n\n";
 	cout << "pixelShuffle doing done\n";
 	//pixelShuffle(dataBlobs[1], dataBlobs[2], 2);
 	//convolution(dataBlobs[1], g_pFilters[1], dataBlobs[2], false);
-	//show(dataBlobs[13]);
+	show(dataBlobs[13]);
 
 }
