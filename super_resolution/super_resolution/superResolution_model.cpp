0aca2905ac0aafc0de06e3ac8ae79948bd151eb3
“学良 <287175720@qq.com>
2021-09-09 09:40:48 +0800

    添加测试时间的代码

--- super_resolution/super_resolution/superResolution_model.cpp
+++ super_resolution/super_resolution/superResolution_model.cpp
@@ -18,6 +18,18 @@ using namespace cv;
 using namespace std;
 #define compare(a) ((a)>=(255)?(255):((a)<(0)?(0):(a)))
 
+#if 1
+cv::TickMeter cvtm;
+#define TIME_START cvtm.reset();cvtm.start();
+#define TIME_END(FUNCNAME) cvtm.stop(); printf(FUNCNAME);printf("=%gms\n", cvtm.getTimeMilli());
+
+#else
+#define TIME_START
+#define TIME_END(FUNCNAME)
+#endif
+
+
+
 extern ConvInfoStruct param_pConvInfo[NUM_CONV_LAYER];
 Filters<float> g_pFilters[NUM_CONV_LAYER];
 void show(CDataBlob<float>& dataBlobs) {
@@ -25,7 +37,7 @@ void show(CDataBlob<float>& dataBlobs) {
 	//Mat result1(dataBlobs[1].rows, dataBlobs[1].cols, CV_8UC3);
 	Mat result2(dataBlobs.rows, dataBlobs.cols, CV_8UC3);
 	Mat image(dataBlobs.rows, dataBlobs.cols, CV_8UC3);
-	cout << dataBlobs.rows << " and " << dataBlobs.cols << " and " << dataBlobs.channels << endl;
+	//cout << dataBlobs.rows << " and " << dataBlobs.cols << " and " << dataBlobs.channels << endl;
 	for (int r = 0; r < result2.rows; r++)
 	{
 		
@@ -36,16 +48,16 @@ void show(CDataBlob<float>& dataBlobs) {
 			{
 				int idx = c * 2 + ch;
 		
-				//cout << compare(dataBlobs[1].getElement(r, c, ch)) <<endl;
+				////cout << compare(dataBlobs[1].getElement(r, c, ch)) <<endl;
 				//p1[idx] =compare(dataBlobs[1].getElement(r, c, ch));
 				result2.at<Vec3b>(r,c)[ch] = compare(dataBlobs.getElement(r, c, 2 - ch) * 255.0);
 			}
 
-			//cout << endl;
+			////cout << endl;
 		}
 	}
 	image = result2;
-	cout << "image.channels=" << image.channels() << endl;
+	//cout << "image.channels=" << image.channels() << endl;
 	for (int ch = 0; ch < image.channels(); ch++) {
 		cout << "The after output \n" << ch << " channel= (\n" << (int)image.at<Vec3b>(0, 0)[ch] << "\t" << (int)image.at<Vec3b>(0, 1)[ch] << " ... " << (int)image.at<Vec3b>(0, image.cols - 1)[ch] << endl
 			<< (int)image.at<Vec3b>(1, 0)[ch] << "\t" << (int)image.at<Vec3b>(1, 1)[ch] << " ... " << (int)image.at<Vec3b>(1, image.cols - 1)[ch] << endl
@@ -60,9 +72,9 @@ void show(CDataBlob<float>& dataBlobs) {
 	//resizeWindow("conv2", dataBlobs[2].cols / 1.8, dataBlobs[2].rows / 1.8);
 
 	//imshow("conv1", result1);
-	imwrite("./srx2.png", result2);
+	//imwrite("./srx2.png", result2);
 	imshow("conv2", result2);
-	waitKey(0);
+	//waitKey(0);
 }
 
 bool param_initialized = false;
@@ -70,7 +82,7 @@ bool param_initialized = false;
 void init_parameters() {
 	for (int i = 0; i < NUM_CONV_LAYER; i++) {
 #if test1112
-		cout << i <<" filter" << endl;
+		//cout << i <<" filter" << endl;
 #endif
 		g_pFilters[i] = param_pConvInfo[i];
 	}
@@ -82,48 +94,49 @@ void superResolution(unsigned char* rgbImageData, int width, int height, int ste
 
 	if (!param_initialized) {
 #if test1112
-		std::cout << "doing param_initialized" << endl;
+		//cout << "doing param_initialized" << endl;
 #endif
 		init_parameters();
 	}
-
+	TIME_START
 	//convert Mat to CDataBlob
 	dataBlobs[0].convertDatatoCDataBlob(rgbImageData, width, height, 3, step);
-	cout << dataBlobs[0] << endl;
+	//cout << dataBlobs[0] << endl;
 	
 
 	//head
-	cout << "head conv\n";
+	//cout << "head conv\n";
 	convolution(dataBlobs[0], g_pFilters[0], dataBlobs[1], false);
-	//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\nhead conv\n" << dataBlobs[1] << "\n\n\n\n\n\n";
+	////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\nhead conv\n" << dataBlobs[1] << "\n\n\n\n\n\n";
 	//show(dataBlobs[1]);
-	cout << "head conv done\n";
+	//cout << "head conv done\n";
 	//simple_blocks x10
-	cout << "simple_blocks x10 conv\n";
+	//cout << "simple_blocks x10 conv\n";
 	int temp = 1;
 	for(int i =1;i<=10;i++){
-		cout << i <<"," << temp << ", " << temp + 1 << ',' << temp + 2 << ',' << temp + 3 << ", " << i + 1 << endl;
+		//cout << i <<"," << temp << ", " << temp + 1 << ',' << temp + 2 << ',' << temp + 3 << ", " << i + 1 << endl;
 		convolutionforsimpleblocks(dataBlobs[i], g_pFilters[temp], g_pFilters[temp +1], g_pFilters[temp +2], g_pFilters[temp +3], dataBlobs[i+1], true);
 		PixelAdd(dataBlobs[i], dataBlobs[i + 1]);
-		//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n 1个 simple_blocks \n" << dataBlobs[1] << "\n\n\n\n\n\n";
-		//cout << dataBlobs[i] << endl << endl;
+		////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n 1个 simple_blocks \n" << dataBlobs[1] << "\n\n\n\n\n\n";
+		////cout << dataBlobs[i] << endl << endl;
 		temp = temp+4;//1111111111111111111111111111111111111111111111111111
 	}
-	cout << "simple_blocks x10 conv done\n";
+	//cout << "simple_blocks x10 conv done\n";
 	/*convolutionforsimpleblocks(dataBlobs[1], g_pFilters[1], g_pFilters[2], g_pFilters[3], g_pFilters[4], dataBlobs[2], true);
 	convolutionforsimpleblocks(dataBlobs[2], g_pFilters[5], g_pFilters[6], g_pFilters[7], g_pFilters[8], dataBlobs[3], true);
 	convolutionforsimpleblocks(dataBlobs[3], g_pFilters[9], g_pFilters[10], g_pFilters[11], g_pFilters[12], dataBlobs[4], true);
 	convolutionforsimpleblocks(dataBlobs[4], g_pFilters[13], g_pFilters[14], g_pFilters[15], g_pFilters[16], dataBlobs[5], true);*/
-	cout << "before pix conv\n";
+	//cout << "before pix conv\n";
 	convolution(dataBlobs[11], g_pFilters[41], dataBlobs[12], false);	
-	//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n上采样前\n" << dataBlobs[12] << "\n\n\n\n\n\n";
-	cout << "before pix conv done\n";
-	cout << "pixelShuffle doing\n";
+	////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n上采样前\n" << dataBlobs[12] << "\n\n\n\n\n\n";
+	//cout << "before pix conv done\n";
+	//cout << "pixelShuffle doing\n";
 	pixelShuffle(dataBlobs[12], dataBlobs[13], 2);
-	//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n上采样后\n" << dataBlobs[13] << "\n\n\n\n\n\n";
-	cout << "pixelShuffle doing done\n";
+	////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n上采样后\n" << dataBlobs[13] << "\n\n\n\n\n\n";
+	//cout << "pixelShuffle doing done\n";
 	//pixelShuffle(dataBlobs[1], dataBlobs[2], 2);
 	//convolution(dataBlobs[1], g_pFilters[1], dataBlobs[2], false);
+	TIME_END("光超分花费的时间")
 	show(dataBlobs[13]);
 
 }
