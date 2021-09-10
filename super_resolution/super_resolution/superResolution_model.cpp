55ce59ac61042c1e62be0169439d8f88223e0240
“学良 <287175720@qq.com>
2021-09-10 15:46:29 +0800

    添加调试信息，和将10x simple_block全部手动写了开来。

--- super_resolution/super_resolution/superResolution_model.cpp
+++ super_resolution/super_resolution/superResolution_model.cpp
@@ -34,7 +34,7 @@ extern ConvInfoStruct param_pConvInfo[NUM_CONV_LAYER];
 Filters<float> g_pFilters[NUM_CONV_LAYER];
 void show(CDataBlob<float>& dataBlobs) {
 	int val = 13;
-	//Mat result1(dataBlobs[1].rows, dataBlobs[1].cols, CV_8UC3);
+	
 	Mat result2(dataBlobs.rows, dataBlobs.cols, CV_8UC3);
 	Mat image(dataBlobs.rows, dataBlobs.cols, CV_8UC3);
 	//cout << dataBlobs.rows << " and " << dataBlobs.cols << " and " << dataBlobs.channels << endl;
@@ -88,55 +88,89 @@ void init_parameters() {
 	}
 }
 
-void superResolution(unsigned char* rgbImageData, int width, int height, int step)
+float superResolution(unsigned char* rgbImageData, int width, int height, int step)
 {
-	CDataBlob<float> dataBlobs[NUM_CONV_LAYER+1];
+	cv::TickMeter cvtm;
+	
+	CDataBlob<float> dataBlobs[14];
 
 	if (!param_initialized) {
 #if test1112
 		//cout << "doing param_initialized" << endl;
 #endif
+		TIME_START
 		init_parameters();
+		param_initialized = true;
+		TIME_END("init param")
 	}
-	TIME_START
+	//cvtm.reset();
+	//cvtm.start();
 	//convert Mat to CDataBlob
+	TIME_START
 	dataBlobs[0].convertDatatoCDataBlob(rgbImageData, width, height, 3, step);
 	//cout << dataBlobs[0] << endl;
-	
+	TIME_END("convert")
 
 	//head
 	//cout << "head conv\n";
+	TIME_START
 	convolution(dataBlobs[0], g_pFilters[0], dataBlobs[1], false);
+	TIME_END("convert conv")
 	////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\nhead conv\n" << dataBlobs[1] << "\n\n\n\n\n\n";
 	//show(dataBlobs[1]);
 	//cout << "head conv done\n";
 	//simple_blocks x10
 	//cout << "simple_blocks x10 conv\n";
-	int temp = 1;
-	for(int i =1;i<=10;i++){
-		//cout << i <<"," << temp << ", " << temp + 1 << ',' << temp + 2 << ',' << temp + 3 << ", " << i + 1 << endl;
-		convolutionforsimpleblocks(dataBlobs[i], g_pFilters[temp], g_pFilters[temp +1], g_pFilters[temp +2], g_pFilters[temp +3], dataBlobs[i+1], true);
-		PixelAdd(dataBlobs[i], dataBlobs[i + 1]);
-		////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n 1个 simple_blocks \n" << dataBlobs[1] << "\n\n\n\n\n\n";
-		////cout << dataBlobs[i] << endl << endl;
-		temp = temp+4;//1111111111111111111111111111111111111111111111111111
-	}
-	//cout << "simple_blocks x10 conv done\n";
-	/*convolutionforsimpleblocks(dataBlobs[1], g_pFilters[1], g_pFilters[2], g_pFilters[3], g_pFilters[4], dataBlobs[2], true);
+	TIME_START
+	//int temp = 1;
+	//for(int i =1;i<=10;i++){
+	//	//cout << i <<"," << temp << ", " << temp + 1 << ',' << temp + 2 << ',' << temp + 3 << ", " << i + 1 << endl;
+	//	convolutionforsimpleblocks(dataBlobs[i], g_pFilters[temp], g_pFilters[temp +1], g_pFilters[temp +2], g_pFilters[temp +3], dataBlobs[i+1], true);
+	//	PixelAdd(dataBlobs[i], dataBlobs[i + 1]);
+	//	
+	//	////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n 1个 simple_blocks \n" << dataBlobs[1] << "\n\n\n\n\n\n";
+	//	////cout << dataBlobs[i] << endl << endl;
+	//	temp = temp+4;//1111111111111111111111111111111111111111111111111111
+	//}
+	convolutionforsimpleblocks(dataBlobs[1], g_pFilters[1], g_pFilters[2], g_pFilters[3], g_pFilters[4], dataBlobs[2], true);
+	PixelAdd(dataBlobs[1], dataBlobs[2]);
 	convolutionforsimpleblocks(dataBlobs[2], g_pFilters[5], g_pFilters[6], g_pFilters[7], g_pFilters[8], dataBlobs[3], true);
+	PixelAdd(dataBlobs[2], dataBlobs[3]);
 	convolutionforsimpleblocks(dataBlobs[3], g_pFilters[9], g_pFilters[10], g_pFilters[11], g_pFilters[12], dataBlobs[4], true);
-	convolutionforsimpleblocks(dataBlobs[4], g_pFilters[13], g_pFilters[14], g_pFilters[15], g_pFilters[16], dataBlobs[5], true);*/
+	PixelAdd(dataBlobs[3], dataBlobs[4]);
+	convolutionforsimpleblocks(dataBlobs[4], g_pFilters[13], g_pFilters[14], g_pFilters[15], g_pFilters[16], dataBlobs[5], true);
+	PixelAdd(dataBlobs[4], dataBlobs[5]);
+	convolutionforsimpleblocks(dataBlobs[5], g_pFilters[17], g_pFilters[18], g_pFilters[19], g_pFilters[20], dataBlobs[6], true);
+	PixelAdd(dataBlobs[5], dataBlobs[6]);
+	convolutionforsimpleblocks(dataBlobs[6], g_pFilters[21], g_pFilters[22], g_pFilters[23], g_pFilters[24], dataBlobs[7], true);
+	PixelAdd(dataBlobs[6], dataBlobs[7]);
+	convolutionforsimpleblocks(dataBlobs[7], g_pFilters[25], g_pFilters[26], g_pFilters[27], g_pFilters[28], dataBlobs[8], true);
+	PixelAdd(dataBlobs[7], dataBlobs[8]);
+	convolutionforsimpleblocks(dataBlobs[8], g_pFilters[29], g_pFilters[30], g_pFilters[31], g_pFilters[32], dataBlobs[9], true);
+	PixelAdd(dataBlobs[8], dataBlobs[9]);
+	convolutionforsimpleblocks(dataBlobs[9], g_pFilters[33], g_pFilters[34], g_pFilters[35], g_pFilters[36], dataBlobs[10], true);
+	PixelAdd(dataBlobs[9], dataBlobs[10]);
+	convolutionforsimpleblocks(dataBlobs[10], g_pFilters[37], g_pFilters[38], g_pFilters[39], g_pFilters[40], dataBlobs[11], true);
+	PixelAdd(dataBlobs[10], dataBlobs[11]);
+
+
+	TIME_END("10 simple_blocks")
+	//cout << "simple_blocks x10 conv done\n";
 	//cout << "before pix conv\n";
+	TIME_START
 	convolution(dataBlobs[11], g_pFilters[41], dataBlobs[12], false);	
+	TIME_END("上采样前卷积花费")
 	////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n上采样前\n" << dataBlobs[12] << "\n\n\n\n\n\n";
 	//cout << "before pix conv done\n";
 	//cout << "pixelShuffle doing\n";
+	TIME_START
 	pixelShuffle(dataBlobs[12], dataBlobs[13], 2);
+	TIME_END("up")
 	////cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n上采样后\n" << dataBlobs[13] << "\n\n\n\n\n\n";
 	//cout << "pixelShuffle doing done\n";
 	//pixelShuffle(dataBlobs[1], dataBlobs[2], 2);
 	//convolution(dataBlobs[1], g_pFilters[1], dataBlobs[2], false);
-	TIME_END("光超分花费的时间")
+	cvtm.stop();
 	show(dataBlobs[13]);
-
+	return cvtm.getTimeMilli();
 }
