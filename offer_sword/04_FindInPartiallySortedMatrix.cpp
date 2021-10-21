a1a2b2cf593d9350ba2a9a1ab2baee0ae39f8407
“学良 <287175720@qq.com>
2021-10-21 21:38:02 +0800

    注意矩阵传递的指针，如果传一级指针，则要用r*col+c，如果传二级指针，则可以matrix[r][c]??

---
+++ offer_sword/04_FindInPartiallySortedMatrix.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+
+// 面试题4：二维数组中的查找
+// 题目：在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按
+// 照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个
+// 整数，判断数组中是否含有该整数。
+
+
+//为什么不是传两星指针？
+bool find_violent(int* matrix ,int row,int col,int number){
+    for(int r=0;r<row;r++){
+        for(int c=0;c<col;c++){
+            if(matrix[r*col+c]==number)
+
+                return true;
+        }
+
+    }
+    return false;
+}
+
+void test_find_violent()
+{
+    int matrix[9]={1,2,8,
+                      2,4,9,
+                      4,7,10};
+    bool a=find_violent(matrix,3,3,11);
+    std::cout<<a;
+}
+
+
+bool Find(int* matrix,int rows, int cols, int number){
+    if(matrix==nullptr||rows<=0||cols<=0)
+        return false;
+    
+    int r =0;
+    int c= cols-1;//从第一行最后一列开始对比
+    //由于需要对总行列数进行改变，所以就不用for了。
+    while(r<rows&&c>=0){//设定一个条件能够执行下去的。r有上界，c有下界.
+        if(matrix[r*rows+c]==number)
+            return true;
+        else if (matrix[r*cols+c] > number )//如果数太大了，说明该列不能要了，要往左边找。
+            --c;
+        else //如果数太小了，说明该行不能要了，要往下面找。
+            ++r;
+
+
+    }
+    return false;
+}
+// ====================测试代码====================
+void Test(char* testName, int* matrix, int rows, int columns, int number, bool expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    bool result = Find(matrix, rows, columns, number);
+    if(result == expected)
+        printf("Passed.\n");
+    else
+        printf("Failed.\n");
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数在数组中
+void Test1()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test1", (int*)matrix, 4, 4, 7, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数不在数组中
+void Test2()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test2", (int*)matrix, 4, 4, 5, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数是数组中最小的数字
+void Test3()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test3", (int*)matrix, 4, 4, 1, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数是数组中最大的数字
+void Test4()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test4", (int*)matrix, 4, 4, 15, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数比数组中最小的数字还小
+void Test5()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test5", (int*)matrix, 4, 4, 0, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数比数组中最大的数字还大
+void Test6()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test6", (int*)matrix, 4, 4, 16, false);
+}
+
+// 鲁棒性测试，输入空指针
+void Test7()
+{
+    Test("Test7", nullptr, 0, 0, 16, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+
+    return 0;
+}
+// int main(){
+//     test_find_violent();
+// }
